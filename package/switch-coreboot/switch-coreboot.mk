cbc0d19882c5743d6f321da5b8efd39af2f57e7c
connor rigby <connor@farmbot.io>
2018-04-24 16:52:21 -0700

    One more time.

--- package/switch-coreboot/switch-coreboot.mk
+++ package/switch-coreboot/switch-coreboot.mk
@@ -11,7 +11,7 @@ HOST_SWITCH_COREBOOT_SUBMODULES = yes
 HOST_SWITCH_COREBOOT_DEPENDENCIES = toolchain uboot host-imx-usb-loader host-arm-linux-gnueabi host-shofel2
 
 define HOST_SWITCH_COREBOOT_BUILD_CMDS
-  cp $(NERVES_DEFCONFIG_DIR)/tegra_mtc.bin $(BINARIES_DIR)
+  cp $(NERVES_DEFCONFIG_DIR)/tegra_mtc.bin $(@D)
   cp $(NERVES_DEFCONFIG_DIR)/package/switch-coreboot/nerves_nintendo_switch_defconfig $(@D)/configs/nerves_nintendo_switch_defconfig
   PATH=$(PATH):$(HOST_DIR)/arm_tc/bin $(MAKE) -C $(@D) nerves_nintendo_switch_defconfig
   PATH=$(PATH):$(HOST_DIR)/arm_tc/bin $(MAKE) -C $(@D) iasl
